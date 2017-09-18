b343224822016ae82e1ab4256cf8afd265b17a5d
Julier Nicolas <bjulin@bbc-be.local>
2017-09-18 16:21:19 +0200

    Home

--- view/default_index.php
+++ view/default_index.php
@@ -1,1 +1,2 @@
-Hallo Welt!
+<p class="home">Hello Everybody,  i welcome you on our website to manage passwords. I hope the site fulfills its purpose and help them.
+Have a nice day an and thank you for using our webiste. </p>
