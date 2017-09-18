961b05cb87320c3afc9ca6602354ace62549d958
Julier Nicolas <bjulin@bbc-be.local>
2017-09-18 13:07:22 +0200

    LoginController

--- view/user_create.php
+++ view/user_create.php
@@ -1,33 +1,33 @@
 <form class="form-horizontal" action="/user/doCreate" method="post">
 	<div class="component" data-html="true">
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="firstName">Vorname</label>
+		  <label class="col-md-2 control-label" for="firstName">Name</label>
 		  <div class="col-md-4">
-		  	<input id="firstName" name="firstName" type="text" placeholder="Vorname" class="form-control input-md">
+		  	<input id="firstName" name="firstName" type="text" placeholder="Name" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="lastName">Nachname</label>
+		  <label class="col-md-2 control-label" for="lastName">Surname</label>
 		  <div class="col-md-4">
-		  	<input id="lastName" name="lastName" type="text" placeholder="Nachname" class="form-control input-md">
+		  	<input id="lastName" name="lastName" type="text" placeholder="Surname" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
 		  <label class="col-md-2 control-label" for="email">Mail</label>
 		  <div class="col-md-4">
-		  	<input id="email" name="email" type="text" placeholder="Mail" class="form-control input-md">
+		  	<input id="email" name="email" type="text" placeholder="example@example.com" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="password">Passwort</label>
+		  <label class="col-md-2 control-label" for="password">Password</label>
 		  <div class="col-md-4">
-		  	<input id="password" name="password" type="password" placeholder="Passwort" class="form-control input-md">
+		  	<input id="password" name="password" type="password" placeholder="Password" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
 	      <label class="col-md-2 control-label" for="send">&nbsp;</label>
 		  <div class="col-md-4">
-		    <input id="send" name="send" type="submit" class="btn btn-primary">
+		    <input id="send" name="send" type="submit" value="Send" class="btn btn-primary">
 		  </div>
 		</div>
 	</div>
