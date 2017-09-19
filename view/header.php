b2962f00a6ffb7e4cc60da28dd06f138503905e7
Abazi Labion <babazl@bbc-be.local>
2017-09-19 09:11:24 +0200

    Rahmen

--- view/header.php
+++ view/header.php
@@ -38,9 +38,6 @@
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
               <li class="active">
-                <a class="headerlink" href="/">Contact<span class="sr-only">(current)</span></a>
-              </li>
-              <li>
                 <a class="headerlink" href="#">Passwordlist<span class="sr-only">(current)</span></a>
               </li>
               <li>
--- view/user_index.php
+++ view/user_index.php
@@ -1,52 +1,63 @@
-<form class="form-horizontal" action="/user/doLogin">
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Username">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Sign in</button>
-    </div>
-  </div>
-</form>
 
+
+  <form class="form-horizontal" action="/user/doLogin">
+    <div class="rahmen">
+    <div class="form-group">
+      <label for="username" class="col-sm-2 control-label">Username</label>
+      <div class="col-sm-10">
+        <input type="username" class="form-control" id="inpuUsername" placeholder="Username">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Sign in</button>
+      </div>
+    </div>
+</div>
+  </form>
 
 <form class="form-horizontal" action="/user/doCreate">
+<div class=rahmen>
 	<div class="form-group">
-		<label for="inputPassword3" class="col-sm-2 control-label">Name</label>
+		<label for="name" class="col-sm-2 control-label">Name</label>
 		<div class="col-sm-10">
-			<input type="name" class="form-control" id="inputPassword3" placeholder="Name">
+			<input type="name" class="form-control" id="inputName3" placeholder="Name">
 		</div>
 	</div>
+
 	<div class="form-group">
-		<label for="inputPassword3" class="col-sm-2 control-label">Surname</label>
+		<label for="name" class="col-sm-2 control-label">Surname</label>
 		<div class="col-sm-10">
-			<input type="name" class="form-control" id="inputPassword3" placeholder="Surname">
+			<input type="name" class="form-control" id="inputName3" placeholder="Surname">
 		</div>
 	</div>
+
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+    <label for="username" class="col-sm-2 control-label">Username</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Username">
+      <input type="username" class="form-control" id="inputUsername3" placeholder="Username">
     </div>
   </div>
+
   <div class="form-group">
     <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
     </div>
   </div>
+
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default">Sign in</button>
     </div>
   </div>
+</div>
 </form>
