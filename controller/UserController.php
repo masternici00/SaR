f1ab1a5301faad84fa12a20ddcaaf011ad74e0ee
Julier Nicolas <bjulin@bbc-be.local>
2017-09-20 08:52:05 +0200

    controller

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <?php
 
 /**
@@ -36,3 +37,55 @@ class UserController
       }
     }
 }
+=======
+<?php
+
+/**
+ * Siehe Dokumentation im DefaultController.
+ */
+class UserController
+{
+    public function login()
+    {
+
+      $view = new View('user_login');
+      $view->title = 'Login';
+      $view->heading = 'Login';
+      $view->display();
+    }
+
+    public function singin()
+    {
+      $view = new View('user_singin');
+      $view->title = 'Sing In';
+      $view->heading = 'Sing In';
+      $view->display();
+    }
+
+    public function doLogin()
+    {
+      $userRepository = new UserRepository();
+      $loggedIn = false;
+      foreach ($userRepository->readAll() as $user) {
+        if ($user->email == $_POST['username']){
+          if ($user->password == shal($_POST['password'])) {
+            $_SESSION['logged_in_user'] = $user->id;
+            $loggedIn = true;
+          }
+        }
+      }
+    }
+    public function doCreate()
+    {
+      $password = sha1($password);
+       $query = "INSERT INTO $this->tableName (firstname, lastname, username, password) VALUES (?, ?, ?, ?)";
+       $statement = ConnectionHandler::getConnection()->prepare($query);
+       $statement->bind_param('ssss', $firstname, $lastname, $username, $password);
+
+       if (!$statement->execute()) {
+           throw new Exception($statement->error);
+       }
+       return $statement->insert_id;
+   }
+ }
+>>>>>>> before discard
